4a37df54296d357ea077271d946b1b0ba96a0ecf
trilokmhl <48346388+Trilokmhl@users.noreply.github.com>
2021-03-13 16:18:11 +0530

    Home Page

---
+++ resources/Js/app.js
@@ -0,0 +1,5 @@
+
+console.log('====================================');
+
+console.log("Hello from Resources App.js page");
+console.log('====================================');
--- server.js
+++ server.js
@@ -5,15 +5,15 @@ const ejs= require('ejs')
 const path= require('path')
 const expLayout= require('express-ejs-layouts')
 
+app.use(express.static('public'))  
+
 app.get('/' ,(req, res)=>{
      res.render('home')
 })
 app.use(expLayout)
 
-// app.set('views', path.join(__dirname,'/resources/views/'))
-
-// app.set('views engine', ejs)
 app.set('views', path.join(__dirname, '/resources/views/'));
+
 app.set('view engine', 'ejs');
 
 app.listen(PORT, ()=>{
